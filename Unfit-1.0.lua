2b589a45259d3783fde174f7528f616ef393bbd0
Jaliborc <contact@jaliborc.com>
2013-04-14 13:22:45 -0400

    Updated shield ID

--- Unfit-1.0.lua
+++ Unfit-1.0.lua
@@ -32,25 +32,25 @@ local _, Class = UnitClass('player')
 local Unusable = Lib.unusable
 
 if Class == 'DEATHKNIGHT' then
-	Unusable = {{3, 4, 10, 11, 13, 14, 15, 16}, {6}} -- weapons, armor, dual wield
+	Unusable = {{3, 4, 10, 11, 13, 14, 15, 16}, {7}} -- weapons, armor, dual wield
 elseif Class == 'DRUID' then
-	Unusable = {{1, 2, 3, 4, 8, 9, 14, 15, 16}, {4, 5, 6}, true}
+	Unusable = {{1, 2, 3, 4, 8, 9, 14, 15, 16}, {4, 5, 7}, true}
 elseif Class == 'HUNTER' then
 	Unusable = {{5, 6, 16}, {5, 6}}
 elseif Class == 'MAGE' then
-	Unusable = {{1, 2, 3, 4, 5, 6, 7, 9, 11, 14, 15}, {3, 4, 5, 6}, true}
+	Unusable = {{1, 2, 3, 4, 5, 6, 7, 9, 11, 14, 15}, {3, 4, 5, 7}, true}
 elseif Class == 'MONK' then
-	Unusable = {{2, 3, 4, 6, 9, 13, 14, 15, 16}, {4, 5, 6}}
+	Unusable = {{2, 3, 4, 6, 9, 13, 14, 15, 16}, {4, 5, 7}}
 elseif Class == 'PALADIN' then
 	Unusable = {{3, 4, 10, 11, 13, 14, 15, 16}, {}, true}
 elseif Class == 'PRIEST' then
-	Unusable = {{1, 2, 3, 4, 6, 7, 8, 9, 11, 14, 15}, {3, 4, 5, 6}, true}
+	Unusable = {{1, 2, 3, 4, 6, 7, 8, 9, 11, 14, 15}, {3, 4, 5, 7}, true}
 elseif Class == 'ROGUE' then
 	Unusable = {{2, 6, 7, 9, 10, 16}, {4, 5, 6}}
 elseif Class == 'SHAMAN' then
 	Unusable = {{3, 4, 7, 8, 9, 14, 15, 16}, {5}}
 elseif Class == 'WARLOCK' then
-	Unusable = {{1, 2, 3, 4, 5, 6, 7, 9, 11, 14, 15}, {3, 4, 5, 6}, true}
+	Unusable = {{1, 2, 3, 4, 5, 6, 7, 9, 11, 14, 15}, {3, 4, 5, 7}, true}
 elseif Class == 'WARRIOR' then
 	Unusable = {{16}, {}}
 end
